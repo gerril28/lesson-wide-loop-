cd5c87a20b247c140145661247f98710e05ca09f
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-05 08:59:58 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    counter += -1
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += 1
+    basic.showNumber(counter)
+})
+let counter = 0
+basic.showIcon(IconNames.EigthNote)
+counter = 0
+basic.showNumber(counter)
+basic.forever(function () {
+    while (false) {
+    	
+    }
+})
